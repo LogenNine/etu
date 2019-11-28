5d41020f9d168feb3aa0f3c2972cc765566ebaa1
LogenNine <39909180+LogenNine@users.noreply.github.com>
2019-11-28 10:49:56 +0300

    Add files via upload

---
+++ Lab2(array).cpp
@@ -0,0 +1,269 @@
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+using namespace std::literals;
+namespace chrono = std::chrono;
+using clock_type = chrono::high_resolution_clock;
+
+const int size = 200;
+int arr[size] = {};
+
+void addElements() {
+	std::cout << "array contains these elements:" << std::endl;
+	for (int i = 0; i < size; ++i) {
+		arr[i] = rand()%100;
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+}//addElements
+
+int partition(int *arr,int start,int end) {
+	int pivot = arr[end];
+	int p_index = start;
+	int i, t;
+	//Here we will check if array value is 
+	//less than pivot
+	//then we will place it at left side
+	//by swapping 
+	for (i = start; i < end; i++) {
+		if (arr[i] < pivot)
+		{
+			t = arr[i];
+			arr[i] = arr[p_index];
+			arr[p_index] = t;
+			p_index++;
+		}//if
+	}//for
+	//Now exchanging value of
+	//pivot and P-index
+	t = arr[end];
+	arr[end] = arr[p_index];
+	arr[p_index] = t;
+	//at last returning the pivot value index
+	return p_index;
+
+}//partition
+
+void quicksort(int *arr, int start,int end) {
+	if (start < end) {
+		int p_index = partition(arr, start, end);
+		quicksort(arr, start, p_index - 1);
+		quicksort(arr, p_index + 1, end);
+	}
+}//quicksort
+
+int averageMatch(int*arr, int start, int end) 
+{
+	int a = 0;
+	int b = 0;
+	a = (arr[start] + arr[end]) / 2;
+	for (int i = 0; i < end;++i) {
+		if (a == arr[i]) ++b;
+	}
+	std::cout << "The average between min and max value of the elements is " << a << std::endl;
+	std::cout << "There are " << b << " elements in the array that are equal to the average between min and max value of the elements" << std::endl;
+	return 0;
+}//averageMatch
+
+int smallerMatch(int a, int* arr, int start, int end)
+{
+	int b = 0;
+	for (int i=start; i<end;++i) 
+	{
+		if (arr[i] < a) ++b;
+	}
+	std::cout << "There are " << b << " elements in the array that are smaller than " << a << std::endl;
+	return 0;
+}//smallerMatch
+
+int biggerMatch(int a, int* arr, int start, int end) 
+{
+	int b = 0;
+	for (int i = start; i < end; ++i)
+	{
+		if (a < arr[i]) ++b;
+	}
+	std::cout << "There are " << b << " elements in the array that are bigger than " << a << std::endl;
+	return 0;
+}
+
+int replaceElement(int a, int b, int* arr, int start, int end) {
+	for (int i = start; i < end; ++i) {
+		if (a == i) arr[i] = b;
+	}
+	return 0;
+}//replaceElement
+
+void deleteElement(unsigned index)
+{
+	if (index >= size) return;
+	for (unsigned u = index + 1; u < size; ++u) {
+		arr[u - 1] = arr[u];
+	}
+}//deleteElement
+
+int searchElement(int a, int* arr, int start, int end) 
+{
+	for (int i = start; i < end; ++i) {
+		if (a == i) std::cout << arr[i] << " lies at that index\n";
+	}
+	return 0;
+}//deleteElement
+
+int swapElement(int a, int b, int* arr) {
+	int temp1 = arr[a];
+	arr[a] = arr[b];
+	arr[b] = temp1;
+	return 0;
+}
+
+int symArray(int* arr, int size) {
+	for (int i = 0; i < size / 2; ++i) {
+		arr[size - i] = arr[size];
+	}
+	for (int i = 0; i < size; ++i) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+	return 0;
+}
+
+int main()
+{
+	int inputNum = 0;
+	int inputForDel = 0;
+	int inputSpotRepl = 0;
+	int inputForRepl = 0;
+	int inputForSearch = 0;
+	int indForSwap1 = 0;
+	int indForSwap2 = 0;
+	char inputSpec;
+	srand((unsigned int) time(0));
+	addElements();
+	quicksort(arr, 0, size - 1);
+	std::cout << "after the quicksort array looks like this:" << std::endl;
+	for (int i = 0; i < size; ++i) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "smallest element is: " << arr[0] << " and biggest element is: " << arr[size - 1] << std::endl << std::endl;
+	averageMatch(arr, 0, size - 1);
+	for (;;) {
+		std::cout << "In this step you will find how many elements in the array are bigger or smaller than the number you did input:" << std::endl;
+		std::cout << "Now enter any number from " << 0 << " to " << INT_MAX << std::endl;
+		std::cin >> inputNum;
+		if (0 < inputNum && INT_MAX > inputNum) break;
+		std::cout << "Error, wrong input, try to input any number from " << 0 << " to " << INT_MAX << std::endl;
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+	}
+	for(;;){
+		std::cout << "do you want to find how many numbers are bigger than yours or how many numbers are smaller?" << std::endl;
+		std::cout << "Input either 'b' for 'bigger' or 's' for 'smaller' to specify what you want:" << std::endl;
+		std::cin >> inputSpec;
+		switch (inputSpec) {
+
+		case 'b': {
+			biggerMatch(inputNum, arr, 0, size - 1);
+			break;
+		}
+		case 's': {
+			smallerMatch(inputNum, arr, 0, size - 1);
+			break;
+		}
+		default:
+		{
+			std::cout << "error wrong input, try inputting 'b' or 's'" << std::endl;
+			std::cin.clear();
+			std::cin.ignore(INT_MAX, '\n');
+			continue;
+		}
+		}
+		break;
+	}
+	for (;;) {
+		std::cout << "In this step you will replace element at the selected index:" << std::endl;
+		std::cout << "chose where to insert the element, input number from 0 to " << size - 1 << std::endl;
+		std::cin >> inputSpotRepl;
+		if (0 < inputSpotRepl && size - 1 > inputSpotRepl) break;
+		std::cout << "Error, wrong input, try to input any number from " << 0 << " to " << size - 1 << std::endl;
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+	}
+	for (;;) {
+		std::cout << "input the number with which to replace the element from the selected spot, it must be bigger than " << INT_MIN << " and smaller than " << INT_MAX << std::endl;
+		std::cin >> inputForRepl;
+		if (INT_MIN < inputForRepl && INT_MAX > inputForRepl) break;
+		std::cout << "Error, wrong input, try to input any number from " << INT_MIN << " to " << INT_MAX << std::endl;
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+	}
+	auto t1 = clock_type::now();
+	replaceElement(inputSpotRepl, inputForRepl, arr, 0, size - 1);
+	auto t2 = clock_type::now();
+	auto interval = t2 - t1;
+	std::cout << "speed of replacing element " << chrono::duration_cast<chrono::nanoseconds>(interval).count() << "ns\n";
+	for (;;) {
+		std::cout << "chose which element to delete, input number from 0 to " << size - 1 << std::endl;
+		std::cin >> inputForDel;
+		if (0 < inputForDel && size - 1 > inputForDel) break;
+		std::cout << "Error, wrong input, try to input any number from " << 0 << " to " << size - 1 << std::endl;
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+	}
+	auto t3 = clock_type::now();
+	deleteElement(inputForDel);
+	auto t4 = clock_type::now();
+	auto interval2 = t4 - t3;
+	std::cout << "speed of deleting element " << chrono::duration_cast<chrono::nanoseconds>(interval2).count() << "ns\n";
+	for (;;) {
+		std::cout << "input the index of the element you want to retrieve, it must be smaller than " << size - 1 << std::endl;
+		std::cin >> inputForSearch;
+		if (0 < inputForSearch && size - 1 > inputForSearch) break;
+		std::cout << "Error, wrong input, try to input any number from " << 0 << " to " << size - 1 << std::endl;
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+	}
+	auto t5 = clock_type::now();
+	searchElement(inputForSearch, arr, 0, size - 1);
+	auto t6 = clock_type::now();
+	auto interval3 = t6 - t5;
+	std::cout << "speed of searching for element " << chrono::duration_cast<chrono::nanoseconds>(interval3).count() << "ns\n";
+	for (;;) {
+		std::cout << "Input two indexes to swap elements that lie at those indexes, inputs must be bigger than 0 and smaller that " << size - 1 << std::endl;
+		std::cin >> indForSwap1 >> indForSwap2;
+		if (0 < indForSwap1 && 0 < indForSwap2 && size - 1 > indForSwap1 && size - 1 > indForSwap2) break;
+		std::cout << "Error, wrong input, try to input numbers from " << 0 << " to " << size - 1 << std::endl;
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+	}
+	std::cout << "At index " << indForSwap1 << " there is number " << arr[indForSwap1] << " and at index " << indForSwap2 << " there is number " << arr[indForSwap2] << std::endl;
+	swapElement(indForSwap1, indForSwap2, arr);
+	std::cout << "We changed those elements and now at index " << indForSwap1 << " there is number " << arr[indForSwap1] << " and at index " << indForSwap2 << " there is number " << arr[indForSwap2] << std::endl;
+	//making symmetrical array
+	symArray(arr, size);
+	std::cout << "We made the array symmetrical:" << std::endl;
+	std::cout << "You can now replace any element and the array and it will remain symmetrical" << std::endl;
+	for (;;) {
+		std::cout << "chose where to insert the element, input number from 0 to " << size - 1 << std::endl;
+		std::cin >> inputSpotRepl;
+		if (0 < inputSpotRepl && size - 1 > inputSpotRepl) break;
+		std::cout << "Error, wrong input, try to input any number from " << 0 << " to " << size - 1 << std::endl;
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+	}
+	for (;;) {
+		std::cout << "input the number with which to replace the element from the selected spot, it must be bigger than " << INT_MIN << " and smaller than " << INT_MAX << std::endl;
+		std::cin >> inputForRepl;
+		if (INT_MIN < inputForRepl && INT_MAX > inputForRepl) break;
+		std::cout << "Error, wrong input, try to input any number from " << INT_MIN << " to " << INT_MAX << std::endl;
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+	}
+	std::cout << "As you can see the array is still symmetrical:" << std::endl;
+	symArray(arr, size);
+	return 0;
+}
+
+//TODO: fix the timer, it always grows
---
+++ intBit.cpp
@@ -0,0 +1,172 @@
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+//This function converts numbers into binary code
+void bitRep(long long int* ap, int aByteSize, char prefix)
+{
+	long long int a = *ap;
+	int aBitSize = aByteSize * 8;
+	long long int b = a & (1LL << (aBitSize - 1));
+	cout << (b == 0 ? 0 : 1) << " ";
+	//color code starts
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+	WORD saved_attributes;
+	/* Save current attributes */
+	GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+	saved_attributes = consoleInfo.wAttributes;
+	SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+	//color code ends
+	if (prefix == 'f' || prefix == 'F')
+	{
+		for (int i = aBitSize - 2; i >= aBitSize-2-7; --i) {
+			long long int b = a & (1LL << i);
+			cout << (b == 0 ? 0 : 1);
+			//	if ((i & 3) == 0) cout << " ";
+		} // for
+		cout << " ";
+		for (int i = aBitSize - 2-7-1; i >= 0; --i) {
+			long long int b = a & (1LL << i);
+			cout << (b == 0 ? 0 : 1);
+			//	if ((i & 3) == 0) cout << " ";
+		} // for
+	}
+	else if (prefix == 'd' || prefix == 'D')
+	{
+		for (int i = aBitSize - 2; i >= aBitSize - 2 - 10; --i) {
+			long long int b = a & (1LL << i);
+			cout << (b == 0 ? 0 : 1);
+			//	if ((i & 3) == 0) cout << " ";
+		} // for
+		cout << " ";
+		for (int i = aBitSize - 2 - 10 - 1; i >= 0; --i) {
+			long long int b = a & (1LL << i);
+			cout << (b == 0 ? 0 : 1);
+			//	if ((i & 3) == 0) cout << " ";
+		} // for
+	}
+	else
+	{
+		for (int i = aBitSize - 2; i >= 0; --i) {
+			long long int b = a & (1LL << i);
+			cout << (b == 0 ? 0 : 1);
+			//	if ((i & 3) == 0) cout << " ";
+		} // for
+	} // else
+	SetConsoleTextAttribute(hConsole, saved_attributes); //return color to normal
+    cout << endl; 
+}//bitRep
+
+//This function handles wrong number inputs from user
+template<typename Type>
+bool Fail(Type min, Type max)
+{
+	char next = cin.peek();
+	if (cin.fail() || next != '\n') {
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cout << endl << "Try to enter correct number from  " << min << " to " << max << endl;
+		return true;
+	}
+	return false;
+}//Fail
+
+//This function randomly changes selected bit
+bool bitRep2(long long int* ap, int aByteSize, char prefix) //todo return numbers back to decimals
+{
+	long long int a = *ap;
+	int bitNumber = 0;
+	int bitNumberMax = aByteSize * 8 - 1;
+	cout << "Now enter bit number in range from 0 to " << bitNumberMax << endl;
+	cin >> bitNumber;
+	if (Fail<int>(0, bitNumberMax)) return false;
+	if (bitNumber < 0 || bitNumber>bitNumberMax) {
+		cout << "Wrong input, bit number should be in range 0 to " << bitNumberMax << endl;
+		return false;
+	}
+	int bitValue = rand() > RAND_MAX / 2 ? 1 : 0;
+	long long int bit = 1LL << bitNumber;
+	if ((a & bit) != 0 && bitValue == 0) a = a & ~bit;
+	if ((a & bit) == 0 && bitValue == 1) a = a | bit;
+	cout << bitValue << " This is random value for selected bit" << endl;
+	//int b = ~bit;
+	//bitRep((long long int*) & b, sizeof(b), prefix);
+	bitRep((long long int*) & a, aByteSize, prefix);
+	return true;
+}//bitRep2
+
+
+void help() {
+	cout << "Prefix i or I means int, after it you can type arabic number no smaller than " << INT_MIN << " and no bigger than " << INT_MAX << endl;
+	cout << "Prefix s or S means short int, after it you can type arabic number no smaller than " << SHRT_MIN << " and no bigger than " << SHRT_MAX << endl;
+	cout << "Prefix u or U means unsigned int, after it you can type arabic number no smaller than " << 0 << " and no bigger than " << UINT_MAX << endl;
+	cout << "Prefix f or F means float, after it you can type arabic number no smaller than " << FLT_MIN << " and no bigger than " << FLT_MAX << endl;
+	cout << "Prefix d or D means double, after it you can type arabic number no smaller than " << DBL_MIN << " and no bigger than " << DBL_MAX << endl;
+	cout << "Correct inputs: i34, U499999, f3.1415, d0.9999999, D1.23e-269"<< endl;
+	cout << "Wrong inputs: int thirty two, CHETIRE, asdasdasd, S4e+2, u-99, d9.9e-9999, s 3, help, /;%$&* "<<endl;
+}
+
+
+//In main we get inputs from user and relay it to functions
+int main() {
+	for (;;)
+	{
+		help();
+		cout << endl << "Enter prefix followed by number:" << endl;
+		char prefix = 0;
+		cin >> prefix;
+		switch (prefix)
+		{
+		case 'i': case 'I': {
+			int a = 0;
+			cin >> a;
+			if (Fail<int>(INT_MIN, INT_MAX)) continue;
+			bitRep((long long int*)& a, sizeof(a), prefix);
+			if (!bitRep2((long long int*) & a, sizeof(a), prefix)) continue;
+			break;
+		}
+		case 's': case 'S':{
+			short int si = 0;
+			cin >> si;
+			if (Fail<short>(SHRT_MIN, SHRT_MAX)) continue;
+			bitRep((long long int*) & si, sizeof(si), prefix);
+			if (!bitRep2((long long int*) & si, sizeof(si), prefix)) continue;
+			break;
+		}
+		case 'u': case 'U': {
+			unsigned int ui = 0;
+			cin >> ui;
+			if (Fail<unsigned>(0, UINT_MAX)) continue;
+			bitRep((long long int*) & ui, sizeof(ui), prefix);
+			if (!bitRep2((long long int*) & ui, sizeof(ui), prefix)) continue;
+			break;
+		}
+		case 'f': case 'F': {
+			float f = 0.0F;
+			cin >> f;
+			if (Fail<float>(FLT_MIN, FLT_MAX)) continue;
+			bitRep((long long int*) & f, sizeof(f), prefix);
+			if (!bitRep2((long long int*) & f, sizeof(f), prefix)) continue;
+			break;
+		}
+		case 'd': case 'D': {
+			double d = 0;
+			cin >> d;
+			if (Fail<double>(DBL_MIN, DBL_MAX)) continue;
+			bitRep((long long int*) & d, sizeof(d), prefix);
+			if (!bitRep2((long long int*) & d, sizeof(d), prefix)) continue;
+			break;
+		}
+		default : 
+		{
+			cout << endl << "error: wrong prefix" << endl << endl;
+			cin.clear();
+			cin.ignore(INT_MAX, '\n');
+			continue;
+		}
+		} //switch
+	} // for
+	return 0;
+}
