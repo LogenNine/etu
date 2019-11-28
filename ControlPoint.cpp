65ad2203b7cd102bbed50d3abbfc4860f4284500
LogenNine <39909180+LogenNine@users.noreply.github.com>
2019-11-29 00:11:39 +0300

    Add files via upload

--- ControlPoint.cpp
+++ ControlPoint.cpp
@@ -6,6 +6,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <chrono>
+#include <vector>
 using namespace std;
 
 int registration() {
@@ -69,7 +70,6 @@ int login() {
 		}
 	}
 	//cout << "Поздравляем! Вы вошли в систему!" << endl << "Нажмите enter для того чтобы продолжить...";
-	//if (_getch() == 13)	return 1;
 	return 1;
 }
 
@@ -161,20 +161,20 @@ int welcome(int logCheck) {
 	return 0;
 }
 
-void arrayFill(int arr[], const int size, int darr[],int sizeD, int choice1, int choice2) {
+void arrayFill(int arr[], const int size, int darr[], int sizeD, int choice1, int choice2) {
 	if (choice1 == 0) {
 		if (choice2 == 0) {
 			cout << "Введите " << sizeD << " чисел, для ввода числа нажмите enter" << endl;
 			for (int i = 0; i < sizeD; i++) cin >> darr[i];
 		}
 		if (choice2 == 1) {
-			for (int i = 0; i < sizeD; i++) darr[i] = rand();
+			for (int i = 0; i < sizeD; i++) darr[i] = rand()%100;
 		}
 		if (choice2 == 2) {
 			ofstream fout;
 			fout.open("array.txt");
 			for (int i = 0; i < sizeD; i++) {
-				fout << rand() << " ";
+				fout << rand()%100 << " ";
 			}
 			fout.close();
 			int num = 0;
@@ -192,13 +192,13 @@ void arrayFill(int arr[], const int size, int darr[],int sizeD, int choice1, int
 			for (int i = 0; i < size; i++) cin >> arr[i];
 		}
 		if (choice2 == 1) {
-			for (int i = 0; i < size; i++) arr[i] = rand();
+			for (int i = 0; i < size; i++) arr[i] = rand()%100;
 		}
 		if (choice2 == 2) {
 			ofstream fout;
 			fout.open("array.txt");
 			for (int i = 0; i < size; i++) {
-				fout << rand() << " ";
+				fout << rand()%100 << " ";
 			}
 			fout.close();
 			int num = 0;
@@ -212,12 +212,192 @@ void arrayFill(int arr[], const int size, int darr[],int sizeD, int choice1, int
 	}
 }
 
+int partition(int arr[],int start, int end) {
+	int pivot = arr[end];
+	int p_index = start;
+	int t;
+	for (int i = start; i < end; i++) {
+		if (arr[i] < pivot) {
+			t = arr[i];
+			arr[i] = arr[p_index];
+			arr[p_index] = t;
+			p_index++;
+		}
+	}
+	t = arr[end];
+	arr[end] = arr[p_index];
+	arr[p_index] = t;
+	return p_index;
+}
+
+void quicksort(int arr[], int start, int end) {
+	if (start < end) {
+		int p_index = partition(arr, start, end);
+		quicksort(arr, start, p_index - 1);
+		quicksort(arr, p_index + 1, end);
+	}
+}
+
+void arraySort(int choice1, int arr[], const int size, int darr[], int sizeD, int choice3) {
+	int tempSize = 0;
+	if (choice1 == 0) {
+		if (choice3 == 0) { //bubble
+			for (int i = 0; i < sizeD; i++) {
+				for (int j = i + 1; j < sizeD; j++) {
+					if (darr[j] < darr[i]) swap(darr[i], darr[j]);
+				}
+			}
+		} // choice3 == 0 
+		if (choice3 == 1) { //shaker
+			tempSize = sizeD;
+			for (int i = 0; i < tempSize;) {
+				for (int j = i + 1; j < tempSize; j++) {
+					if (darr[j] < darr[j-1]) swap(darr[j], darr[j - 1]);
+				}
+				tempSize--;
+				for (int k = tempSize - 1; k > i; k--) {
+					if (darr[k] < darr[k - 1]) swap(darr[k], darr[k - 1]);
+				}
+				i++;
+			}
+		}//choice3 == 1
+		if (choice3 == 2) {//Comb sort
+			int gap = sizeD;
+			bool flag = true;
+			while (gap != 1 || flag == true) {
+				gap = (gap * 10) / 13;
+				if (gap < 1) gap = 1;
+				flag = false;
+				for (int i = 0; i < sizeD - gap; i++) {
+					if (darr[i] > darr[i + gap]) {
+						swap(darr[i], darr[i + gap]);
+						flag = true;
+					}
+				}
+			}
+		}//choice3 == 2
+		if (choice3 == 3) {//insert sort
+			int key, j;
+			for (int i = 1; i < sizeD; i++) {
+				key = darr[i];
+				j = i;
+				while (j > 0 && darr[j - 1] > key) {
+					darr[j] = darr[j - 1];
+					j--;
+				}
+				darr[j] = key;
+			}
+		}//choice3 == 3
+		if (choice3 == 4) {//quick sort
+			quicksort(darr, 0, sizeD - 1);			
+		}//choice3 == 4
+	}
+	if (choice1 == 1) {
+		if (choice3 == 0) { //bubble
+			for (int i = 0; i < size; i++) {
+				for (int j = i + 1; j < size; j++) {
+					if (arr[j] < arr[i]) swap(arr[i], arr[j]);
+				}
+			}
+		} // choice3 == 0 
+		if (choice3 == 1) { //shaker
+			tempSize = size;
+			for (int i = 0; i < tempSize;) {
+				for (int j = i + 1; j < tempSize; j++) {
+					if (arr[j] < arr[j - 1]) swap(arr[j], arr[j - 1]);
+				}
+				tempSize--;
+				for (int k = tempSize - 1; k > i; k--) {
+					if (arr[k] < arr[k - 1]) swap(arr[k], arr[k - 1]);
+				}
+				i++;
+			}
+		}//choice3 == 1
+		if (choice3 == 2) {//Comb sort
+			int gap = size;
+			bool flag = true;
+			while (gap != 1 || flag == true) {
+				gap = (gap * 10) / 13;
+				if (gap < 1) gap = 1;
+				flag = false;
+				for (int i = 0; i < size - gap; i++) {
+					if (arr[i] > arr[i + gap]) {
+						swap(arr[i], arr[i + gap]);
+						flag = true;
+					}
+				}
+			}
+		}//choice3 == 2
+		if (choice3 == 3) {//insert sort
+			int key, j;
+			for (int i = 1; i < size; i++) {
+				key = arr[i];
+				j = i;
+				while (j > 0 && arr[j - 1] > key) {
+					arr[j] = arr[j - 1];
+					j--;
+				}
+				arr[j] = key;
+			}
+		}//choice3 == 3
+		if (choice3 == 4) {//quick sort
+			quicksort(arr, 0, size - 1);
+		}//choice3 == 4
+	}
+}
+
+int fib(int fibArr[], int arr[], int fib) {
+	int a = 0, b = 1;
+	fibArr[0] = a;
+	fibArr[1] = b;
+	for (int i = 2; i < 30; i++) {
+		fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+	}
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 30; j++) {
+			if (fibArr[j] == arr[i]) {
+				fib++;
+				break;
+			}
+		}
+	}
+	cout << "Из первых 15 чисел отсортированного массива " << fib << " являются числами Фибоначчи, а ";
+	cout << 15 - fib << " не являются числами Фибоначчи" << endl;
+	return 0;
+}
+
+int averageAndMedian(int arr[], int size, int median, int average, int moda) {
+	int temp = 0;
+	int count = 0;
+	median = arr[size / 2];
+	for (int i = 0; i < size; i++) {
+		temp += arr[i];
+		count++;
+	}
+	average = temp / count;
+	count = 0;
+	int z = 0;
+	for (int n = 0; n < size; n++) {
+		for (int p = 0; p < size; p++) {
+			if (arr[n] == arr[p]) count++;
+		}
+		if (count > 1) {
+			if (count > z) {
+				z = count;
+				moda = arr[n];
+			}
+			count = 0;
+		}
+	}
+
+	cout << "Среднее значение массива = " << average << " медиана = " << median << " а мода = " << moda << endl;
+	return 0;
+}
+
 int arraysMenu() {
 	const int size = 100;
-	int arr[size], code = 0, key = 0, fib = 0, noFib = 0, sizeD = 0;
+	int arr[size], code = 0, key = 0, fibC = 0, sizeD = 0, fibArr[30], median = 0, average = 0, moda = 0;
 	int choice1, choice2, choice3;
-	int* darr = new int[sizeD];
-	double time = 0;
 	while (code != 13) {
 		system("cls");
 		key = (key + 2) % 2;
@@ -234,7 +414,8 @@ int arraysMenu() {
 	if (choice1 == 0) {
 		cout << "Введите размер динамического массива:" << endl;
 		cin >> sizeD;
-	}	
+	}
+	int* darr = new int[sizeD];
 	code = 0;
 	key = 0;
 	while (code != 13) {
@@ -284,12 +465,32 @@ int arraysMenu() {
 		if (code == 72 || code == 119) key--;
 	}
 	choice3 = key;
+	chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
+	arraySort(choice1, arr, size, darr, sizeD, choice3);
+	chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+	chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
 	code = 0;
 	key = 0;
-	cout << "На сортировку массива было потрачено " << time << " секунд" << endl;
-	cout << "Из первых 15 чисел отсортированного массива " << fib << " являются числами Фибоначчи, а";
-	cout << noFib << "не являются числами Фибоначчи" << endl;
-
+	cout << "Так выглядит массив после сортировки: " << endl;
+	if (choice1 == 0) {
+		for (int i = 0; i < sizeD; i++) cout << darr[i] << " ";
+		cout << endl;
+	}
+	if (choice1 == 1) {
+		for (int i = 0; i < size; i++) cout << arr[i] << " ";
+		cout << endl;
+	}
+	cout << "На сортировку массива было потрачено " << time_span.count() << " секунд" << endl;
+	if(choice1 == 0) fib(fibArr, darr, fibC);
+	if (choice1 == 1) fib(fibArr, arr, fibC);
+	//for (int i = 0; i < 30; i++) cout << fibArr[i] << " ";
+	if (choice1 == 0) averageAndMedian(darr, sizeD, median, average, moda);
+	if (choice1 == 1) averageAndMedian(arr, size, median, average, moda);	
+	cout << endl;
+	while (code != 13) {
+		cout << "Нажмите enter чтобы вернутся в главное меню..." << endl;
+		code = _getch();
+	}
 	return 0;
 }
 
@@ -330,12 +531,9 @@ int menu(int logCheck) {
 		if (key == 4) cout << "-> Выход" << endl;
 		else cout << "   Выход" << endl;
 
-		//code = _getch();
-		//if (code == 224) {
 			code = _getch();
 			if (code == 80 || code == 115) key++;
 			if (code == 72 || code == 119) key--;
-		//}
 	}
 	system("cls");
 	return key;
@@ -343,7 +541,7 @@ int menu(int logCheck) {
 
 int main()
 {
-	srand(time(NULL));
+	srand((unsigned int)time(NULL));
 	setlocale(0, "");
 	int logCheck = 0;
 	for (;;) {
